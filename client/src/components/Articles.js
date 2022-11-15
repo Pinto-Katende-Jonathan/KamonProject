17c77c606e61ea75c4fc5dfe964d565d676eab8e
Jonathan Katende Pinto <79837754+Pinto-Katende-Jonathan@users.noreply.github.com>
2022-11-15 09:47:58 +0200

    Delete client directory

--- client/src/components/Articles.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Articles() {
-  return (
-    <div>Articles</div>
-  )
-}
-
-export default Articles
--- client/src/components/Gerer_Factures.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function GererFactures() {
-  return (
-    <div>Gerer toutes les  Factures</div>
-  )
-}
-
-export default GererFactures
